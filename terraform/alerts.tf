460165f6859b8ee51f3c6337ab07b7924093750b
rohit <rohit.yadav@bluepi.in>
2024-04-08 11:32:29 +0530

    tf

--- terraform/alerts.tf
+++ terraform/alerts.tf
@@ -11,7 +11,7 @@ resource "azurerm_monitor_action_group" "ActionGroupDemo" {
 resource "azurerm_monitor_metric_alert" "storage_account_alert" {
   name                = "storage-account-capacity-alert-${var.DEPARTMENT}-${var.PROJECT}-${var.ENV}"
   resource_group_name = module.bluepi.rg_name
-  scopes              = module.bluepi_storage.storage_name
+  scopes              = [module.bluepi_storage.storage_name]
 
   criteria {
     metric_namespace = "Microsoft.Storage/storageAccounts"

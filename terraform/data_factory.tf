a7e53f2e30a22570a153056cd22380568f13266c
rohit <rohit.yadav@bluepi.in>
2024-04-08 17:07:59 +0530

    test

--- terraform/data_factory.tf
+++ terraform/data_factory.tf
@@ -1,25 +1,25 @@
-#module "bluepi_data_factory" {
- # source  = "app.terraform.io/rohityad/azure_datafactory/azurerm"
-  #version = "1.0.25"
+module "bluepi_data_factory" {
+  source  = "app.terraform.io/rohityad/azure_datafactory/azurerm"
+  version = "1.0.25"
 
-  #application         = "bluepi-${var.ENV}"
-  #costcenter          = var.COSTCENTER
-  #department          = var.DEPARTMENT
-  #owner               = var.OWNER
-  #env                 = var.ENV
-  #project_name        = var.PROJECT
-  #resource_group_name = module.bluepi.rg_name
-  #data_factory_name   = "DF-${var.PROJECT}-${var.ENV}01"
-  #location            = data.azurerm_location.current.display_name
-  #github_configuration = {
+  application         = "bluepi-${var.ENV}"
+  costcenter          = var.COSTCENTER
+  department          = var.DEPARTMENT
+  owner               = var.OWNER
+  env                 = var.ENV
+  project_name        = var.PROJECT
+  resource_group_name = module.bluepi.rg_name
+  data_factory_name   = "DF-${var.PROJECT}-${var.ENV}01"
+  location            = data.azurerm_location.current.display_name
+  github_configuration = {
 
-   # account_name    = "tf-bluep"
-   # branch_name     = "main"
-   # git_url         = "https://github.com/"
-   # repository_name = "tf-infra"
-   # root_folder     = "/"
-  #}
-  #integration_runtime_type        = "Azure"
-  #integration_runtime_custom_name = "WebAnalytics-${var.PREFIX}SHIR-01"
+    account_name    = "tf-bluep"
+    branch_name     = "main"
+    git_url         = "https://github.com/"
+    repository_name = "tf-infra"
+    root_folder     = "/"
+  }
+  integration_runtime_type        = "Azure"
+  integration_runtime_custom_name = "WebAnalytics-${var.PREFIX}SHIR-01"
 
-#}
+}

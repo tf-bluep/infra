20737b0454e557a8222c861bcb73f82e101a6308
rohit <rohit.yadav@bluepi.in>
2024-04-09 00:05:11 +0530

    test

--- terraform/pipeline_alerts.tf
+++ terraform/pipeline_alerts.tf
@@ -1,21 +1,21 @@
-resource "azurerm_monitor_metric_alert" "pipeline_failure_alert" {
-  name                 = "pipeline_failure_alert-${var.project}-${var.env}"
-  resource_group_name  = module.bluepi.rg_name
-  scopes               = [module.bluepi_data_factory.data_factory_id]
+#resource "azurerm_monitor_metric_alert" "pipeline_failure_alert" {
+ # name                 = "pipeline_failure_alert-${var.project}-${var.env}"
+ # resource_group_name  = module.bluepi.rg_name
+ # scopes               = [module.bluepi_data_factory.data_factory_id]
   #evaluation_frequency = "Minute"
-  frequency            = "PT1M" 
-  severity             = "3" 
+ # frequency            = "PT1M" 
+ # severity             = "3" 
 
-  criteria {
-    metric_namespace = "Microsoft.DataFactory/factories/pipelines"
-    metric_name      = "PipelineRunsFailed"
-    aggregation      = "Total"
-    operator         = "GreaterThan"
-    threshold        = 0
-  }
+  #criteria {
+   # metric_namespace = "Microsoft.DataFactory/factories/pipelines"
+   # metric_name      = "PipelineRunsFailed"
+   # aggregation      = "Total"
+   # operator         = "GreaterThan"
+   # threshold        = 0
+  #}
 
-  action {
-    action_group_id = module.azure_actiongroup.action_group_id
-  }
-}
+  #action {
+   # action_group_id = module.azure_actiongroup.action_group_id
+  #}
+#}
 

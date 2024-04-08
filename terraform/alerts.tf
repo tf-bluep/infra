b6253a193a6b7a76c9ce4bc6bc4e43b4460d3f83
rohit <rohit.yadav@bluepi.in>
2024-04-08 22:27:27 +0530

    test

--- terraform/alerts.tf
+++ terraform/alerts.tf
@@ -33,3 +33,21 @@ resource "azurerm_monitor_metric_alert" "storage_account_alert" {
   description = "Alert triggered when Used capacity exceeds 1%"
   window_size       = "PT1H"
 }
+resource "azurerm_monitor_metric_alert" "pipeline_failure_alert" {
+  name                 = "pipeline_failure_alert-${var.project}-${var.env}"
+  resource_group_name  = module.bluepi.rg_name
+  scopes               = module.prism_data_factory.data_factory_id
+  evaluation_frequency = "Minute"
+  frequency            = 2 
+  severity             = "5" 
+
+  criteria {
+    metric_namespace = "Microsoft.DataFactory/factories/pipelines"
+    metric_name      = "PipelineRunsFailed"
+    aggregation      = "Total"
+    operator         = "GreaterThan"
+    threshold        = 0
+  }
+  action {
+    action_group_id = azurerm_monitor_action_group.ActionGroupDemo.id
+  }

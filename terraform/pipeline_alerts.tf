ac1f43b93619d175c04ee0fe5a0f2b2fc7d75d77
rohit <rohit.yadav@bluepi.in>
2024-04-08 23:01:30 +0530

    test

--- terraform/pipeline_alerts.tf
+++ terraform/pipeline_alerts.tf
@@ -15,7 +15,7 @@ resource "azurerm_monitor_metric_alert" "pipeline_failure_alert" {
   }
 
   action {
-    action_group_id = azurerm_monitor_action_group.this.id
+    action_group_id = action_group_id
   }
 }
 

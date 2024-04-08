afdbc1238ab4f43584fbc9bcbd188e53c66528db
rohit <rohit.yadav@bluepi.in>
2024-04-08 22:43:04 +0530

    test

--- terraform/pipeline_alerts.tf
+++ terraform/pipeline_alerts.tf
@@ -1,7 +1,7 @@
 resource "azurerm_monitor_metric_alert" "pipeline_failure_alert" {
   name                 = "pipeline_failure_alert-${var.project}-${var.env}"
   resource_group_name  = module.bluepi.rg_name
-  scopes               = module.bluepi_data_factory.data_factory_id
+  scopes               = [module.bluepi_data_factory.data_factory_id]
   evaluation_frequency = "Minute"
   frequency            = 2 
   severity             = "5" 

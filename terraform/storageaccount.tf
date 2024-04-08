db8ae2da8850a735b7314ab1e0d01c43053e1a06
rohit <rohit.yadav@bluepi.in>
2024-04-08 10:02:52 +0530

    tf

---
+++ terraform/storageaccount.tf
@@ -0,0 +1,19 @@
+module "bluepi_storage" {
+  source  = "app.terraform.io/rohityad/azure_resourcegroup/azurerm"
+  version = "1.0.7"
+
+  account_kind                    = "StorageV2"
+  account_replication_type        = "LRS"
+  account_tier                    = "Standard"
+  application                     = "${var.PROJECT}-${var.ENV}"
+  costcenter                      = var.COSTCENTER
+  department                      = var.DEPARTMENT
+  env                             = var.ENV
+  location                        = data.azurerm_location.current.display_name
+  owner                           = var.OWNER
+  rgname                          = module.prism.rg_name
+  storage_account_name            = "sa${var.project}df${var.env}"
+  enable_hns                      = true
+  public_network_access_enabled   = true
+  allow_nested_items_to_be_public = true
+}

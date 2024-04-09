1a15a54318771cb94cbedfc121d43f5d76d32e0f
rohit <rohit.yadav@bluepi.in>
2024-04-09 11:00:46 +0530

    test

--- terraform/pipeline_alerts.tf
+++ terraform/pipeline_alerts.tf
@@ -12,8 +12,8 @@ resource "azurerm_monitor_metric_alert" "pipeline_failure_alert" {
   severity             = "3" 
 
   criteria {
-    metric_namespace = "Microsoft.DataFactory/pipeline"
-    metric_name      = "Failed_Pipeline_Runs_Metrics"
+    metric_namespace = "Microsoft.DataFactory/datafactories"
+    metric_name      = "Failed pipeline runs metrics"
     aggregation      = "Total"
     operator         = "GreaterThan"
     threshold        = 0

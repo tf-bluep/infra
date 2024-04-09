60b525df2f543f108aa6a372a1542bc5de11639d
rohit <rohit.yadav@bluepi.in>
2024-04-09 16:57:09 +0530

    test

--- terraform/container.tf
+++ terraform/container.tf
@@ -1,7 +1,7 @@
-#module "azure_containers" {
- # source  = "app.terraform.io/rohityad/azure_containers/azurerm"
- # version = "1.0.0"
+module "azure_containers" {
+  source  = "app.terraform.io/rohityad/azure_containers/azurerm"
+  version = "1.0.0"
 
- # container_names      = ["edh-data-${var.env}"]
- # storage_account_name = module.bluepi_storage.storage_name
-#}
+  container_names      = ["edh-data-${var.env}"]
+  storage_account_name = module.bluepi_storage.storage_name
+}

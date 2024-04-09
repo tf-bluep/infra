24a2a8130aad2d20ef98166c7306f958980a6c6c
rohit <rohit.yadav@bluepi.in>
2024-04-09 12:22:19 +0530

    test

--- terraform/pipeline_alerts.tf
+++ terraform/pipeline_alerts.tf
@@ -7,7 +7,6 @@ resource "azurerm_monitor_metric_alert" "pipeline_failure_alert" {
   name                 = "pipeline_failure_alert-${var.project}-${var.env}"
   resource_group_name  = module.bluepi.rg_name
   scopes               = [data.azurerm_data_factory.pipeline.id]
-  #evaluation_frequency = "Minute"
   frequency            = "PT1M" 
   severity             = "3" 
 
@@ -23,4 +22,44 @@ resource "azurerm_monitor_metric_alert" "pipeline_failure_alert" {
     action_group_id = module.azure_actiongroup.action_group_id
   }
 }
+resource "azurerm_monitor_metric_alert" "pipeline_cancelled_alert" {
+  name                = "PipelineCancelledAlert-${var.project}-${var.env}"
+  resource_group_name = module.bluepi.rg_name
+  scopes              = [data.azurerm_data_factory.pipeline.id]
+  frequency           = "PT1M" 
+  severity            = "3"
+
+  criteria {
+    metric_namespace = "Microsoft.DataFactory/factories"
+    metric_name      = "PipelineCancelledRuns"
+    aggregation      = "Total"
+    operator         = "GreaterThan"
+    threshold        = 0
+  }
+
+  action {
+    action_group_id = module.azure_actiongroup.action_group_id
+  }
+}
+
+resource "azurerm_monitor_metric_alert" "trigger_failure_alert" {
+  name                = "TriggerFailureAlert-${var.project}-${var.env}"
+  resource_group_name = module.bluepi.rg_name
+  scopes              = [data.azurerm_data_factory.pipeline.id]
+  frequency           = "PT1M" 
+  severity            = "3"
+
+  criteria {
+    metric_namespace = "Microsoft.DataFactory/factories"
+    metric_name      = "TriggerFailedRuns"
+    aggregation      = "Total"
+    operator         = "GreaterThan"
+    threshold        = 0
+  }
+
+  action {
+    action_group_id = module.azure_actiongroup.action_group_id
+  }
+}
+
 

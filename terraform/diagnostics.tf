2db18d33369a803356539ad81821e70ad7da1514
rohit <rohit.yadav@bluepi.in>
2024-04-10 16:40:46 +0530

    test

--- terraform/diagnostics.tf
+++ terraform/diagnostics.tf
@@ -1,10 +1,10 @@
-module "azure_diagnostics" {
-  source  = "app.terraform.io/rohityad/azure_diagnostics/azurerm"
-  version = "1.0.0"
+#module "azure_diagnostics" {
+ # source  = "app.terraform.io/rohityad/azure_diagnostics/azurerm"
+  #version = "1.0.0"
   # insert required variables here
 
-  diag_name             = "diag-${var.DEPARTMENT}-${var.PROJECT}-${var.ENV}"
-  logs_destinations_ids = "/subscriptions/f2fb6ada-1520-4d1a-a23b-8ebbcf63a24c/resourceGroups/RG-it-bluepi-dev/providers/Microsoft.Storage/storageAccounts/sabluepidfdev"
-  resource_id           ="module.bluepi_storage.storage_account_id"
-
-}
+  #diag_name             = "diag-${var.DEPARTMENT}-${var.PROJECT}-${var.ENV}"
+  #logs_destinations_ids = "/subscriptions/f2fb6ada-1520-4d1a-a23b-8ebbcf63a24c/resourceGroups/RG-it-bluepi-dev/providers/Microsoft.Storage/storageAccounts/sabluepidfdev"
+  #resource_id           ="module.bluepi_storage.storage_account_id"
+#
+#}

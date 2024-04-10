1dc0cf016edb7d8b9ee0cd53d8895c50576dddd6
rohit <rohit.yadav@bluepi.in>
2024-04-10 12:04:04 +0530

    test

--- terraform/diagnostics.tf
+++ terraform/diagnostics.tf
@@ -5,9 +5,9 @@ module "azure_diagnostics" {
 
   diag_name             = "diag-${var.DEPARTMENT}-${var.PROJECT}-${var.ENV}"
   logs_destinations_ids = [
-  "/subscriptions/f2fb6ada-1520-4d1a-a23b-8ebbcf63a24c/resourceGroups/my-resource-group/providers/Microsoft.Storage/storageAccounts/my-storage-account",
-  "/subscriptions/f2fb6ada-1520-4d1a-a23b-8ebbcf63a24c/resourceGroups/my-resource-group/providers/Microsoft.OperationalInsights/workspaces/my-log-analytics-workspace",
-  "/subscriptions/f2fb6ada-1520-4d1a-a23b-8ebbcf63a24c/resourceGroups/my-resource-group/providers/Microsoft.EventHub/namespaces/my-event-hub-namespace"
+  "/subscriptions/f2fb6ada-1520-4d1a-a23b-8ebbcf63a24c/resourceGroups/my-resource-group/RG-it-bluepi-devs/Microsoft.Storage/storageAccounts/my-storage-account",
+  "/subscriptions/f2fb6ada-1520-4d1a-a23b-8ebbcf63a24c/resourceGroups/my-resource-group/RG-it-bluepi-dev/Microsoft.OperationalInsights/workspaces/my-log-analytics-workspace",
+  "/subscriptions/f2fb6ada-1520-4d1a-a23b-8ebbcf63a24c/resourceGroups/my-resource-group/RG-it-bluepi-dev/Microsoft.EventHub/namespaces/my-event-hub-namespace"
 ]
   resource_id           ="/subscriptions/f2fb6ada-1520-4d1a-a23b-8ebbcf63a24c/resourceGroups/RG-it-bluepi-dev/providers/Microsoft.Storage/storageAccounts/sabluepidfdev"
 

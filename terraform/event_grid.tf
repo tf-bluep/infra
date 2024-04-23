a3e694dd54262cc338b2dc121e49fb11ee3cd754
rohit <rohit.yadav@bluepi.in>
2024-04-23 11:46:40 +0530

    tf

--- terraform/event_grid.tf
+++ terraform/event_grid.tf
@@ -2,11 +2,13 @@ resource "azurerm_eventgrid_topic" "this" {
   name                = "topic-${var.DEPARTMENT}-${var.PROJECT}-${var.ENV}"
   resource_group_name = module.bluepi.rg_name
   location            = data.azurerm_location.current.display_name
+  tags = {
   application         = "${var.PROJECT}-${var.ENV}"
   costcenter          = var.COSTCENTER
   department          = var.DEPARTMENT
   env                 = var.ENV
   owner               = var.OWNER
+  }
 }
 
 #resource "azurerm_eventgrid_event_subscription" "example" {

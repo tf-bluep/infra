5bae0df5777c425bf242609dde292cc2adea57cd
rohit <rohit.yadav@bluepi.in>
2024-04-23 11:39:24 +0530

    tf

---
+++ terraform/event_grid.tf
@@ -0,0 +1,19 @@
+resource "azurerm_eventgrid_topic" "this" {
+  name                = "topic-${var.DEPARTMENT}-${var.PROJECT}-${var.ENV}"
+  resource_group_name = module.bluepi.rg_name
+  location            = data.azurerm_location.current.display_name
+  application         = "${var.PROJECT}-${var.ENV}"
+  costcenter          = var.COSTCENTER
+  department          = var.DEPARTMENT
+  env                 = var.ENV
+  owner               = var.OWNER
+}
+
+#resource "azurerm_eventgrid_event_subscription" "example" {
+ # name                   = "subscription-${var.DEPARTMENT}-${var.PROJECT}-${var.ENV}"
+ # scope                  = azurerm_eventgrid_topic.this.id
+ # event_delivery_schema  = "EventGridSchema"
+ # endpoint_type          = "WebHook"
+ # endpoint_url           = "https://your-webhook-url"
+ # included_event_types   = ["All"]
+#}

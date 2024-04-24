935b408e729d692d5198253d83f9bd0a1acd28d5
rohit <rohit.yadav@bluepi.in>
2024-04-24 16:46:29 +0530

    tf

--- terraform/event_grid.tf
+++ terraform/event_grid.tf
@@ -1,41 +1,23 @@
-#resource "azurerm_eventgrid_topic" "this" {
- # name                = "topic-${var.DEPARTMENT}-${var.PROJECT}-${var.ENV}"
- # resource_group_name = module.bluepi.rg_name
- # location            = data.azurerm_location.current.display_name
- # tags = {
- # application         = "${var.PROJECT}-${var.ENV}"
- # costcenter          = var.COSTCENTER
- # department          = var.DEPARTMENT
- # env                 = var.ENV
- # owner               = var.OWNER
- # }
-#}
-
-
-
 resource "azurerm_storage_queue" "this" {
-  name                  = "saqueue${var.project}df${var.env}"
-  storage_account_name  = module.bluepi_storage.storage_name
- 
+  name                 = "saqueue${var.project}df${var.env}"
+  storage_account_name = module.bluepi_storage.storage_name
 }
 
 resource "azurerm_eventgrid_system_topic" "this" {
   name                   = "topic-${var.DEPARTMENT}-${var.PROJECT}-${var.ENV}"
-  location               = "Global"
+  location               = data.azurerm_location.current.display_name
   resource_group_name    = module.bluepi.rg_name
-  #source_arm_resource_id = module.bluepi.rg_id
+  source_arm_resource_id = module.bluepi.rg_name_id
   topic_type             = "Microsoft.Resources.ResourceGroups"
 }
 
-
 resource "azurerm_eventgrid_system_topic_event_subscription" "this" {
   name                   = "subscription-${var.DEPARTMENT}-${var.PROJECT}-${var.ENV}"
-  #scope                  = module.bluepi_storage.storage_account_id
-  system_topic            = azurerm_eventgrid_system_topic.this.name
-  resource_group_name     = module.bluepi.rg_name
- # event_delivery_schema  = "EventGridSchema"
-  #included_event_types    = ["BlobCreated"]
-  
+  system_topic           = azurerm_eventgrid_system_topic.this.name
+  resource_group_name    = module.bluepi.rg_name
+  event_delivery_schema  = "EventGridSchema"
+  included_event_types   = ["BlobCreated"]
+
   storage_queue_endpoint {
     storage_account_id = module.bluepi_storage.storage_account_id
     queue_name         = azurerm_storage_queue.this.name

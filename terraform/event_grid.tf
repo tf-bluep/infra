39375ff02640081c0ed99e69548eb26e3d8d071b
rohit <rohit.yadav@bluepi.in>
2024-04-24 14:51:53 +0530

    tf

--- terraform/event_grid.tf
+++ terraform/event_grid.tf
@@ -1,15 +1,17 @@
-resource "azurerm_eventgrid_topic" "this" {
-  name                = "topic-${var.DEPARTMENT}-${var.PROJECT}-${var.ENV}"
-  resource_group_name = module.bluepi.rg_name
-  location            = data.azurerm_location.current.display_name
-  tags = {
-  application         = "${var.PROJECT}-${var.ENV}"
-  costcenter          = var.COSTCENTER
-  department          = var.DEPARTMENT
-  env                 = var.ENV
-  owner               = var.OWNER
-  }
-}
+#resource "azurerm_eventgrid_topic" "this" {
+ # name                = "topic-${var.DEPARTMENT}-${var.PROJECT}-${var.ENV}"
+ # resource_group_name = module.bluepi.rg_name
+ # location            = data.azurerm_location.current.display_name
+ # tags = {
+ # application         = "${var.PROJECT}-${var.ENV}"
+ # costcenter          = var.COSTCENTER
+ # department          = var.DEPARTMENT
+ # env                 = var.ENV
+ # owner               = var.OWNER
+ # }
+#}
+
+
 
 resource "azurerm_storage_queue" "this" {
   name                  = "saqueue${var.project}df${var.env}"
@@ -17,14 +19,22 @@ resource "azurerm_storage_queue" "this" {
  
 }
 
+resource "azurerm_eventgrid_system_topic" "this" {
+  name                   = "topic-${var.DEPARTMENT}-${var.PROJECT}-${var.ENV}"
+  location               = "Global"
+  resource_group_name    = module.bluepi.rg_name
+  source_arm_resource_id = module.bluepi.rg_name_id
+  topic_type             = "Microsoft.Resources.ResourceGroups"
+}
+
 
 resource "azurerm_eventgrid_system_topic_event_subscription" "this" {
   name                   = "subscription-${var.DEPARTMENT}-${var.PROJECT}-${var.ENV}"
   #scope                  = module.bluepi_storage.storage_account_id
-  system_topic            = azurerm_eventgrid_topic.this.name
+  system_topic            = azurerm_eventgrid_system_topic.this.name
   resource_group_name     = module.bluepi.rg_name
-  event_delivery_schema  = "EventGridSchema"
-  included_event_types    = ["BlobCreated"]
+ # event_delivery_schema  = "EventGridSchema"
+  #included_event_types    = ["BlobCreated"]
   
   storage_queue_endpoint {
     storage_account_id = module.bluepi_storage.storage_account_id
